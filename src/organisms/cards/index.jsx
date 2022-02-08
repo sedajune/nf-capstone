ccd607b9a84de921b8379265a8c4c1a978e6461f
Silke Groenke <silkigr@gmail.com>
2022-02-08 13:42:59 +0100

    fix: bug fixes

--- src/organisms/cards/index.jsx
+++ src/organisms/cards/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
